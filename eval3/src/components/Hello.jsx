a490b2637b83949fc0b822655d270c8351e46ae3
Ace0012 <ayushchandra.knp@gmail.com>
2024-01-10 21:47:31 +0530

    All done

---
+++ eval3/src/components/Hello.jsx
@@ -0,0 +1,18 @@
+import React from "react"
+import "../styles/styles.css"
+const Hello = ()=>{
+    return (
+        <div>
+            <h1>
+                <center>
+                    Hello this is my first react app 
+                </center>
+            </h1>
+        <p>
+            I want color to be red 
+            
+        </p>
+        </div>
+    )
+}
+export default Hello ;
